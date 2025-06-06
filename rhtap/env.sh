183fc43ad48623a21d5cacbb523f156b75ffcbfa
Jan Smid <124383262+jsmid1@users.noreply.github.com>
2025-06-06 14:08:37 +0200

    Update env.sh

--- rhtap/env.sh
+++ rhtap/env.sh
@@ -6,7 +6,7 @@ CI_TYPE=${CI_TYPE:-jenkins}
 
 # from buildah-rhtap
 TAG=$(git rev-parse HEAD)
-export IMAGE_URL=${IMAGE_URL-artifactory-docker-artifactory-jcr.apps.rosa.rhtap-services.xmdt.p3.openshiftapps.com/rhtap/rhtap-qe-jsmid:$CI_TYPE-$TAG}
+export IMAGE_URL=${IMAGE_URL-artifactory-docker-artifactory-jcr2.apps.rosa.rhtap-services.xmdt.p3.openshiftapps.com/rhtap/rhtap-qe-jsmid:$CI_TYPE-$TAG}
 export IMAGE=${IMAGE-$IMAGE_URL}
 
 export DOCKERFILE=${DOCKERFILE-docker/Dockerfile}
